7b901a6d8615379ccf759b988b2ccb2c7c36e60d
leyuri <leyuri97@gmail.com>
2020-06-01 16:19:06 +0900

    Add swaggerUi

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// swaggerUi
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// 라우팅 걸어주기
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
