40794da444d9671c2d63962432921e03ca6e210e
leyuri <leyuri97@gmail.com>
2020-06-09 03:14:40 +0900

    Fix user model [encode password after creating]

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,21 @@
 'use strict';
+
+const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {});
+  }, {
+    hooks: {
+      // password를 집어넣을 때 암호화를 해서 password를 save하게 될 것
+      beforeCreate: (user) => {
+        user.password = bcrypt.hashSync(user.password, 10);
+      }
+    }
+  });
+  User.prototype.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  }
   User.associate = function(models) {
     // associations can be defined here
   };
--- server/test/models/user.test.js
+++ server/test/models/user.test.js
@@ -1,18 +1,20 @@
 const db = require('../../models');
 
 afterEach(async (done) => {
-    await db.User.destroy({ truncate: true, cascade: true })
-    done();
-  });
-
+  await db.User.destroy({ truncate: true, cascade: true })
+  done();
+});
 
 it('should encode password after creating', async (done) => {
-    const user = await db.User.create({
-      email: '1@com', password: 'password'
-    });
-    // 암호화가 되었으니까 password와 다르기를 기대하는 것
-    expect(user.password).not.toBe('password');
-    expect(user.validatePassword('password')).toBe(true);
-    expect(user.validatePassword('password1')).toBe(false);
-    done();
+  const user = await db.User.create({
+    email: '1@com', password: 'password'
+  });
+
+  // 암호화가 되었으니까 password와 다르기를 기대하는 것
+  expect(user.password).not.toBe('password');
+  console.log(user.password);
+//   sequelize model 에 instance 를 추가
+  expect(user.validatePassword('password')).toBe(true);
+  expect(user.validatePassword('password1')).toBe(false);
+  done();
 });
