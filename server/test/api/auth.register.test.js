88391681c23c09eb79642496e48ddf980a6909ea
leyuri <leyuri97@gmail.com>
2020-06-08 22:46:43 +0900

    Test

--- server/test/api/auth.register.test.js
+++ server/test/api/auth.register.test.js
@@ -22,12 +22,25 @@ it('should return 400 error if email is missing', (done) => {
     request(app).post('/api/auth/register')
         .send({password: 'passpass'})
         .expect(400, done);
-})
+});
 
 it('should return 400 error if password is missing', (done) => {
     request(app).post('/api/auth/register')
         .send({email: 'passpass'})
         .expect(400, done);
-})
+});
+
+// it('should make a new user after registeration', (done) => {
+//     request(app).post('/api/auth/register')
+//         .send({email: 'test@test.com', password: 'passpass'})
+//         .then(resp => {
+//             expect(resp.body.token).toBeTruthy();
+//             done();
+//         })
+// });
+
+
+
+
 
 
