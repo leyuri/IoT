125cbfd0cd0d5e2daeb6aa2a944910fdef7a4039
leyuri <leyuri97@gmail.com>
2020-06-08 22:21:01 +0900

    Fix error

--- server/api/auth.js
+++ server/api/auth.js
@@ -2,6 +2,11 @@ function register(req, res, next) {
     if (!req.body.email) {
         return res.status(400).json({message: 'email is missing'});
     }
+    if (!req.body.password) {
+        return res.status(400).json({message: 'password is missing'});
+    }
+
+
     res.json({token: 'xxxxxx'})
 }
 module.exports = {
